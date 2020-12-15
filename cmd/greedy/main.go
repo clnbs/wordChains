8929be3c977125e25c34f7a667a0c6cb45f420f9
Colin <china_men@protonmail.com>
2020-12-15 18:55:09 +0100

    Change main implementation

--- cmd/greedy/main.go
+++ cmd/greedy/main.go
@@ -6,52 +6,48 @@ import (
 	"os"
 )
 
+func usage(programName string) {
+	fmt.Println("usage :\t\t", programName, "path/to/wordlist.txt word1 word2")
+	fmt.Println("example :\t", programName, "./assets/app/wordlist.txt cat dog")
+}
+
+func printSolutions(solutions [][]string) {
+	if len(solutions) == 0 {
+		fmt.Println("no solution found")
+		return
+	}
+	fmt.Println("found", len(solutions), "solution(s)")
+	for index, chain := range solutions {
+		fmt.Print("solution #", index+1, " : ")
+		for chainIndex, word := range chain {
+			fmt.Print(word)
+			if chainIndex == len(chain)-1 {
+				continue
+			}
+			fmt.Print(" -> ")
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
+	programName, args := os.Args[0], os.Args[1:]
+	if len(args) != 3 {
+		usage(programName)
+		return
+	}
 	solver := wordchainsresolver.NewGreedySolver()
 	factory := wordchainsresolver.NewFileLoaderFactory(os.Getenv("GOPATH") + "/src/github.com/clnbs/wordChains/assets/app/wordlist.txt")
 	wcr := wordchainsresolver.NewWordChainsResolver(solver, factory)
 	err := wcr.LoadDB()
 	if err != nil {
-		panic(err)
+		fmt.Println("error while loading word list :", err)
+		return
 	}
-	path, err := wcr.Solve("ruby", "code")
+	path, err := wcr.Solve(args[1], args[2])
 	if err != nil {
-		panic(err)
+		fmt.Println("error while solving word chains :", err)
+		return
 	}
-	fmt.Println(path)
-	path, err = wcr.Solve("lead", "gold")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
-	path, err = wcr.Solve("gold", "lead")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
-	path, err = wcr.Solve("cat", "dog")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
-	path, err = wcr.Solve("cake", "gold")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
-	path, err = wcr.Solve("bar", "oil")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
-	path, err = wcr.Solve("oil", "bar")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
-	path, err = wcr.Solve("abates", "hollow")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(path)
+	printSolutions(path)
 }
